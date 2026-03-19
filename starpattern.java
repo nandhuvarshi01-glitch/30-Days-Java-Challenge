f415fc42c009adf132b133cb6e4de9afd7b720fc
NANTHINI P <nandhuvarshi01@gmail.com>
2026-03-19 21:13:14 +0530

    Day 09 - Star Pyramid Java program

---
+++ starpattern.java
@@ -0,0 +1,16 @@
+class starpattern{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+(2*i-1);j++){
+                if(j<=n-i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
