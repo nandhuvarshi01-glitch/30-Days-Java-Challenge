ed5281f797f61b9205f3b47766cda29544aef362
NANTHINI P <nandhuvarshi01@gmail.com>
2026-03-20 21:00:18 +0530

    Day 10 - Inverted Pyramid Java program

---
+++ invertedpyr.java
@@ -0,0 +1,16 @@
+class invertedpyr{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=n-i+(2*i-1);j++){
+                if(j<=n-i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
