a0b3d3c13e8a08f0951b3d57dfedc8a507ff0eb9
NANTHINI P <nandhuvarshi01@gmail.com>
2026-03-21 19:51:05 +0530

    Day 11 -Diamond Pyramid Java program

---
+++ diamond.java
@@ -0,0 +1,27 @@
+class diamond{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+(2*i-1);j++){
+                if(j<=n-i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+   for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=n-i+(2*i-1);j++){
+                if(j<=n-i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+} 
