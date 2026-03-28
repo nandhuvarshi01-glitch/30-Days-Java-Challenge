7ada65277e92897afb7628d2c00e23523024185c
NANTHINI P <nandhuvarshi01@gmail.com>
2026-03-28 22:17:00 +0530

    Day 15 -Largest Element in Array Java program

---
+++ maxele.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class maxele{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int max=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
